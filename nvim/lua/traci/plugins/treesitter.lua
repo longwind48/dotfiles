2fc631ea3fdb5ef81428cf299a531e40b01f2db8
longwind48 <longwind48@gmail.com>
2026-02-08 21:42:22 +0800

    fix: misc

--- nvim/lua/traci/plugins/treesitter.lua
+++ nvim/lua/traci/plugins/treesitter.lua
@@ -1,71 +1,54 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- Install parsers
-    local ensure_installed = {
-      "json",
-      "javascript",
-      "typescript",
-      "tsx",
-      "yaml",
-      "html",
-      "css",
-      "markdown",
-      "markdown_inline",
-      "svelte",
-      "graphql",
-      "bash",
-      "lua",
-      "vim",
-      "dockerfile",
-      "gitignore",
-      "query",
-      "vimdoc",
-      "python",
-      "c",
-    }
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	main = "nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"python",
+			"c",
+		},
+		sync_install = false,
+		auto_install = true,
+	},
+	config = function(_, opts)
+		require("nvim-treesitter").setup(opts)
 
-    -- Auto-install missing parsers
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function()
-        local ft = vim.bo.filetype
-        local lang = vim.treesitter.language.get_lang(ft) or ft
-        if vim.tbl_contains(ensure_installed, lang) then
-          pcall(function()
-            if not pcall(vim.treesitter.language.inspect, lang) then
-              vim.cmd("TSInstall " .. lang)
-            end
-          end)
-        end
-      end,
-    })
+		-- Setup autotag
+		require("nvim-ts-autotag").setup()
 
-    -- Enable treesitter highlighting and indentation
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function()
-        pcall(vim.treesitter.start)
-      end,
-    })
+		-- Incremental selection keymaps
+		vim.keymap.set("n", "<C-space>", function()
+			require("nvim-treesitter.incremental_selection").init_selection()
+		end, { desc = "Init treesitter selection" })
 
-    -- Incremental selection keymaps
-    vim.keymap.set("n", "<C-space>", function()
-      require("nvim-treesitter.incremental_selection").init_selection()
-    end, { desc = "Init treesitter selection" })
+		vim.keymap.set("x", "<C-space>", function()
+			require("nvim-treesitter.incremental_selection").node_incremental()
+		end, { desc = "Increment treesitter selection" })
 
-    vim.keymap.set("x", "<C-space>", function()
-      require("nvim-treesitter.incremental_selection").node_incremental()
-    end, { desc = "Increment treesitter selection" })
-
-    vim.keymap.set("x", "<bs>", function()
-      require("nvim-treesitter.incremental_selection").node_decremental()
-    end, { desc = "Decrement treesitter selection" })
-
-    -- Setup autotag
-    require("nvim-ts-autotag").setup()
-  end,
+		vim.keymap.set("x", "<bs>", function()
+			require("nvim-treesitter.incremental_selection").node_decremental()
+		end, { desc = "Decrement treesitter selection" })
+	end,
 }
